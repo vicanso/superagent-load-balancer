b5d6f10c73b49cf20777ed2a0bb2059751dbd8b4
vicanso <vicansocanbico@gmail.com>
2017-03-01 22:18:04 +0800

    update test

--- test/balancer.js
+++ test/balancer.js
@@ -4,129 +4,108 @@ const assert = require('assert');
 const request = require('superagent');
 const Balancer = require('../lib/balancer');
 
-describe('Balancer', () => {
-  it('round-robin balancer', () => {
-    const balancer = new Balancer([
-      {
-        ip: '127.0.0.1',
-        port: 8086,
-      },
-      {
-        ip: '192.168.1.2',
-        port: 8086,
-      },
-    ], 'round-robin');
+function createServer(port) {
+  const http = require('http');
+  const server = http.createServer((req, res) => {
+    res.statusCode = 204;
+    res.end();
+  });
+  server.listen(port);
+}
 
-    assert.equal(balancer.getBackend('/users/me').ip, '127.0.0.1');
-    assert.equal(balancer.getBackend('/users/me').ip, '192.168.1.2');
+describe('Balancer', () => {
+  const backendList = [
+    {
+      ip: '127.0.0.1',
+      port: 8000,
+    },
+    {
+      ip: '127.0.0.1',
+      port: 8001,
+    },
+    {
+      ip: '127.0.0.1',
+      port: 8002,
+    },
+  ];
+  it('init', (done) => {
+    createServer(8000);
+    createServer(8001);
+    createServer(8002);
+    setTimeout(done, 1000);
+  });
+
+  it('round-robin balancer', () => {
+    const balancer = new Balancer(backendList, 'round-robin');
+
+    assert.equal(balancer.getBackend({
+      url: '/users/me',
+    }).port, 8000);
+    assert.equal(balancer.getBackend({
+      url: '/users/me',
+    }).port, 8001);
   });
 
   it('first balancer', () => {
-    const balancer = new Balancer([
-      {
-        ip: '127.0.0.1',
-        port: 8086,
-      },
-      {
-        ip: '192.168.1.2',
-        port: 8086,
-      },
-    ], 'first');
+    const balancer = new Balancer(backendList, 'first');
 
     assert.equal(balancer.getBackend({
       url: '/users/me',
-    }).ip, '127.0.0.1');
+    }).port, 8000);
     assert.equal(balancer.getBackend({
       url: '/users/me',
-    }).ip, '127.0.0.1');
+    }).port, 8000);
   });
 
   it('url balancer', () => {
-    const balancer = new Balancer([
-      {
-        ip: '127.0.0.1',
-        port: 8086,
-      },
-      {
-        ip: '192.168.1.2',
-        port: 8086,
-      },
-    ], 'url');
-
+    const balancer = new Balancer(backendList, 'url');
     assert.equal(balancer.getBackend({
       url: '/users/me',
-    }).ip, '127.0.0.1');
+    }).port, 8001);
     assert.equal(balancer.getBackend({
-      url: '/users/me?no-cache',
-    }).ip, '192.168.1.2');
+      url: '/users/me?cache=false',
+    }).port, 8002);
   });
 
   it('url-path balancer', () => {
-    const balancer = new Balancer([
-      {
-        ip: '127.0.0.1',
-        port: 8086,
-      },
-      {
-        ip: '192.168.1.2',
-        port: 8086,
-      },
-    ], 'url-path');
+    const balancer = new Balancer(backendList, 'url-path');
 
     assert.equal(balancer.getBackend({
       url: '/users/me',
-    }).ip, '127.0.0.1');
+    }).port, 8001);
     assert.equal(balancer.getBackend({
       url: '/users/me?cache=false',
-    }).ip, '127.0.0.1');
+    }).port, 8001);
     assert.equal(balancer.getBackend({
       url: '/users/ab?cache=false',
-    }).ip, '192.168.1.2');
+    }).port, 8000);
   });
 
 
   it('leastconn balancer', () => {
-    const balancer = new Balancer([
-      {
-        ip: '127.0.0.1',
-        port: 8086,
-      },
-      {
-        ip: '192.168.1.2',
-        port: 8086,
-      },
-    ], 'leastconn');
+    const balancer = new Balancer(backendList, 'leastconn');
     const chooser = balancer.chooser;
     let backend = balancer.getBackend({
       url: '/users/me',
     });
-    assert.equal(backend.ip, '127.0.0.1');
+    assert.equal(backend.port, 8000);
     chooser.increase(backend.id);
     assert.equal(balancer.getBackend({
       url: '/users/me',
-    }).ip, '192.168.1.2');
+    }).port, 8001);
     chooser.decrease(backend.id);
     assert.equal(balancer.getBackend({
       url: '/users/me',
-    }).ip, '127.0.0.1');
+    }).port, 8000);
   });
 
 
   it('start health check', (done) => {
-    const balancer = new Balancer([
-      {
-        ip: '127.0.0.1',
-        port: 8086,
-      },
-      {
-        ip: '192.168.1.2',
-        port: 8086,
-      },
-    ]);
+    const balancer = new Balancer(backendList);
     const ping = (backend) => {
       return new Promise((resolve, reject) => {
         setTimeout(() => {
-          if (backend.ip !== '127.0.0.1') {
+          if (backend.port !== 8001) {
             reject(new Error('backend is disabled'));
           } else {
             resolve();
@@ -141,86 +120,59 @@ describe('Balancer', () => {
     setTimeout(() => {
       assert.equal(balancer.getBackend({
         url: '/users/me',
-      }).ip, '127.0.0.1');
+      }).port, 8001);
       assert.equal(balancer.getBackend({
         url: '/users/me',
-      }).ip, '127.0.0.1');
+      }).port, 8001);
       assert.equal(balancer.getBackend({
         url: '/users/me',
-      }).ip, '127.0.0.1');
+      }).port, 8001);
       done();
     }, 1000).unref();
   });
 
 
   it('request by balancer', (done) => {
-    const balancer = new Balancer([
-      {
-        ip: '127.0.0.1',
-        port: 8086,
-      },
-      {
-        host: 'localhost',
-        ip: '127.0.0.1',
-        port: 8086,
-      },
-    ]);
+    const balancer = new Balancer(backendList);
     const plugin = balancer.plugin();
     request.get('/ping')
       .use(plugin)
       .then((res) => {
         assert.equal(res.status, 204);
-        assert.equal(res.request.backendServer.ip, '127.0.0.1');
+        assert.equal(res.request.backendServer.port, 8000);
         return request.get('/ping').use(plugin);
       }).then((res) => {
         assert.equal(res.status, 204);
-        assert.equal(res.request.backendServer.host, 'localhost');
+        assert.equal(res.request.backendServer.port, 8001);
         done();
       }).catch(done);
   });
 
   it('request by leastconn balancer', (done) => {
-    const balancer = new Balancer([
-      {
-        ip: '127.0.0.1',
-        port: 8086,
-      },
-      {
-        host: 'localhost',
-        port: 8086,
-      },
-    ], 'leastconn');
+    const balancer = new Balancer(backendList, 'leastconn');
     const plugin = balancer.plugin();
     request.get('/ping')
       .use(plugin)
       .then((res) => {
         assert.equal(res.status, 204);
-        assert.equal(res.request.backendServer.ip, '127.0.0.1');
+        assert.equal(res.request.backendServer.port, 8000);
         return request.get('/ping').use(plugin);
       }).then((res) => {
         assert.equal(res.status, 204);
-        assert.equal(res.request.backendServer.ip, '127.0.0.1');
+        assert.equal(res.request.backendServer.port, 8000);
         done();
       }).catch(done);
     request.get('/ping')
       .use(plugin)
       .then((res) => {
         assert.equal(res.status, 204);
-        assert.equal(res.request.backendServer.host, 'localhost')
+        assert.equal(res.request.backendServer.port, 8001);
       }).catch(done);
   });
 
   it('request https://www.baidu.com/ by balancer', (done) => {
-    const balancer = new Balancer([
-      {
-        ip: '127.0.0.1',
-        port: 8086,
-      },
-      {
-        host: 'localhost',
-        port: 8086,
-      },
-    ]);
+    // request url is not start with '/', it will no use load balancer
+    const balancer = new Balancer(backendList);
     const plugin = balancer.plugin();
     request.get('https://www.baidu.com/')
       .use(plugin)
@@ -232,46 +184,22 @@ describe('Balancer', () => {
 
 
   it('getAvailableServers ', () => {
-    const balancer = new Balancer([
-      {
-        host: 'localhost',
-        ip: '127.0.0.1',
-        port: 8086,
-      },
-      {
-        ip: '127.0.0.1',
-        port: 8086,
-        backup: true,
-      },
-    ]);
+    const balancer = new Balancer(backendList);
     const serverList = balancer.getAvailableServers();
-    assert.equal(serverList.length, 1);
-    assert.equal(serverList[0].host, 'localhost');
+    assert.equal(serverList.length, 3);
+    assert.equal(serverList[0].port, 8000);
   });
 
   it('addAlgorithm', () => {
-    const balancer = new Balancer([
-      {
-         host: 'localhost',
-         ip: '127.0.0.1',
-         port: 8086,
-      },
-      {
-         ip: '127.0.0.1',
-         port: 8086,
-      },
-    ], 'even');
-    balancer.addAlgorithm('use-backup', (request) => {
+    const balancer = new Balancer(backendList, 'third');
+    balancer.addAlgorithm('third', (request) => {
       const index = Math.floor(Math.random() * 1000);
-      if (index % 2 === 0) {
-        return index;
-      }
-      return index + 1;
+      return Math.floor(index / 3) * 3 + 2;
     });
     for (let i = 0; i < 100; i++) {
       assert.equal(balancer.getBackend({
         url: `${Math.random()}`,
-      }).ip, '127.0.0.1');
+      }).port, 8002);
     }
   });
 });
